0a3a734833519bee0cf0c94c5bca107fd4bb542b
malaartyom <malarenkoart@bk.ru>
2024-02-16 12:39:20 +0700

    first task

---
+++ 1.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int find_double() {
+    double e = 1.0;
+    int counter = 0;
+
+    while (1 + e != 1) {
+        counter++;
+        e /= 2;
+    }
+
+    return counter - 1;
+}
+
+
+double find_half_e() {
+    double e = 1.0;
+    int counter = 0;
+
+    while (1 + e != 1) {
+        counter++;
+        e /= 2;
+    }
+
+    return e;
+}
+
+
+int find_float() {
+    float e = 1.0;
+    int counter = 0;
+
+    while (1 + e != 1) {
+        counter++;
+        e /= 2;
+    }
+
+    return counter - 1;
+}
+
+
+int find_power_double() {
+    double e = 1.0;
+    int counter = 0;
+
+    while (!isinf(e)) {
+        counter++;
+        e *= 2;
+    }
+
+    return counter;
+}
+
+
+int find_power_float() {
+    float e = 1.0;
+    int counter = 0;
+
+    while (!isinf(e)) {
+        counter++;
+        e *= 2;
+    }
+
+    return counter;
+}
+
+
+void compare() {
+    double half_e = find_half_e();
+    double e = half_e * 2;
+
+    if (1.0 == 1.0 + half_e) {
+        printf("1 is equal to 1 + e/2\n");
+    }
+    if (1 + half_e < 1 + e){
+        printf("1 + e/2 less then 1 + e\n");
+    }
+    if (1 + e + half_e > 1 + e){
+        printf("1 + e + e/2 is greater then 1 + e\n");
+    }
+}
+
+
+int find_min(){
+    float e = 1.0;
+    int counter = 0;
+
+    while (e != 0) {
+        counter++;
+        e /= 2;
+    }
+
+    return counter;
+}
+
+
+int main() {
+    compare();
+    return 0;
+}
---
+++ 1.py
@@ -0,0 +1,11 @@
+def e():
+    e = 1.0
+    counter = 0
+    while (1 + e != 1):
+        counter += 1
+        e /= 2
+    
+    return (e, counter)
+
+
+print(e())
